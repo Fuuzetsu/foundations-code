69ea941cd1a5409d2360e74429a5a25fd483ca0a
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-04-23 16:31:52 +0100

    List monoid and monoid endomorphisms under composition

--- code/Usage.java
+++ code/Usage.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,9 +11,29 @@ public class Usage {
             }
         };
 
+        F<Integer, Integer> square = new F<Integer, Integer>() {
+            public Integer apply(Integer x) {
+                return x * x;
+            }
+        };
+
+        F<Integer, Integer> addFive = new F<Integer, Integer>() {
+            public Integer apply(Integer x) {
+                return x + 5;
+            }
+        };
+
+        F<Integer, Integer> timesThree = new F<Integer, Integer>() {
+            public Integer apply(Integer x) {
+                return x * 3;
+            }
+        };
+
+
         Monoid<Integer> intMon = Usage.intAddMonoid();
         Monoid<String> strMon = Usage.stringConcatMonoid();
         Monoid<F<Integer, Integer>> compMon = Usage.funcMonoid();
+        Monoid<List<Integer>> listAppMonoid = Usage.listAppendMonoid();
 
         List<Integer> someInts = new LinkedList<Integer>();
         someInts.add(2);
@@ -36,6 +57,29 @@ public class Usage {
         print.apply("Folding right " + stringList);
         print.apply(strMon.foldr(stringList));
 
+        List<F<Integer, Integer>> someFuncs =
+            new LinkedList<F<Integer, Integer>>();
+        someFuncs.add(square);
+        someFuncs.add(addFive);
+        someFuncs.add(timesThree);
+        FList<F<Integer, Integer>> funcList
+            = new FList<F<Integer, Integer>>(someFuncs);
+        print.apply("Folding left [f x = x * x, f x = x + 5, f x = x * 3]");
+        Integer x = compMon.foldl(funcList).apply(7);
+        print.apply("Applying the result of the fold to 7: " + x);
+        print.apply("Folding right [f x = x * x, f x = x + 5, f x = x * 3]");
+        Integer y = compMon.foldr(funcList).apply(7);
+        print.apply("Applying the result of the fold to 7: " + y);
+
+        List<List<Integer>> someLists = new LinkedList<List<Integer>>();
+        someLists.add(Arrays.asList(1, 3, 4));
+        someLists.add(Arrays.asList(25, 7, 16));
+        someLists.add(Arrays.asList(735, 17, 8));
+        FList<List<Integer>> listList = new FList<List<Integer>>(someLists);
+        print.apply("Folding left " + someLists);
+        print.apply(listAppMonoid.foldl(listList));
+        print.apply("Folding right " + someLists);
+        print.apply(listAppMonoid.foldr(listList));
 
     }
 
@@ -68,6 +112,25 @@ public class Usage {
             }
         };
         return Monoid.monoid(concat, "");
+    }
+
+    public static <A> Monoid<List<A>> listAppendMonoid() {
+        F<List<A>, F<List<A>, List<A>>> append =
+            new F<List<A>, F<List<A>, List<A>>>() {
+            public F<List<A>, List<A>> apply(final List<A> xs) {
+                F<List<A>, List<A>> f = new F<List<A>, List<A>>() {
+                    public List<A> apply(final List<A> ys) {
+                        /* Make sure we don't mutate the original lists */
+                        List<A> nl = new LinkedList<A>();
+                        nl.addAll(xs);
+                        nl.addAll(ys);
+                        return nl;
+                    }
+                };
+                return f;
+            }
+        };
+        return Monoid.monoid(append, new LinkedList<A>());
     }
 
 
